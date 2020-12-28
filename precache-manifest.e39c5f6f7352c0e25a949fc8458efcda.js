cd59f381fdc16ca608b52c7ee29273e15ac7ef00
anthonyolores <tonyoolores@gmail.com>
2020-12-29 00:51:33 +1300

    Updates

---
+++ precache-manifest.e39c5f6f7352c0e25a949fc8458efcda.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12f59e72f95235583e424644b434fa1f",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "ec8fdad45287e102c4ee",
+    "url": "/covid19/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "78e38df5e2603fea63f7",
+    "url": "/covid19/static/css/main.ab6f2ce5.chunk.css"
+  },
+  {
+    "revision": "ec8fdad45287e102c4ee",
+    "url": "/covid19/static/js/2.df21ab6f.chunk.js"
+  },
+  {
+    "revision": "13767bda7e58db6e95d0e3d841e04d61",
+    "url": "/covid19/static/js/2.df21ab6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78e38df5e2603fea63f7",
+    "url": "/covid19/static/js/main.f835a3ea.chunk.js"
+  },
+  {
+    "revision": "3cdf9652cdb863df465b",
+    "url": "/covid19/static/js/runtime-main.7f1725b7.js"
+  }
+]);
